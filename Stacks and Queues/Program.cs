ae079b97cdc89566595bc8bdd199ba7f2015f20b
balint.csertan <balint.csertan@pazirik.hu>
2021-12-04 22:53:53 +0100

    Orderprocessing with queue

--- Stacks and Queues/Program.cs
+++ Stacks and Queues/Program.cs
@@ -58,7 +58,45 @@ namespace Stacks_and_Queues
                 Console.WriteLine("DeQueueed item: {0}", queue.Dequeue());
             }
 
+            Queue<Order> ordersQueue = new Queue<Order>();
 
+            foreach (Order order in ReceiveOrdersFromBranch1())
+            {
+                ordersQueue.Enqueue(order);
+            }
+
+            foreach (Order order in ReceiveOrdersFromBranch2())
+            {
+                ordersQueue.Enqueue(order);
+            }
+
+            while (ordersQueue.Count > 0)
+            {
+                Order currentOrder = ordersQueue.Dequeue();
+                currentOrder.ProcessOrder();
+            }
+        }
+
+        static Order[] ReceiveOrdersFromBranch1()
+        {
+            Order[] orders = new Order[]
+            {
+                new Order(1,5),
+                new Order(2,4),
+                new Order(6,10),
+            };
+            return orders;
+        }
+
+        static Order[] ReceiveOrdersFromBranch2()
+        {
+            Order[] orders = new Order[]
+            {
+                new Order(3,5),
+                new Order(4,4),
+                new Order(5,10),
+            };
+            return orders;
         }
     }
 
