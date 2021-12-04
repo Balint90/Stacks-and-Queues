22c3d657cc41c4c68479399050d6e644667a884a
balint.csertan <balint.csertan@pazirik.hu>
2021-12-04 14:54:19 +0100

    Reversing an array's numbers

--- Stacks and Queues/Program.cs
+++ Stacks and Queues/Program.cs
@@ -14,6 +14,32 @@ namespace Stacks_and_Queues
             Console.WriteLine("Top value in the stack is: {0}", stack.Peek());
             stack.Push(3);
             Console.WriteLine("Top value in the stack is: {0}", stack.Peek());
+            int item = stack.Pop();
+            Console.WriteLine("Popped item: {0}", item);
+            Console.WriteLine("Top value in the stack is: {0}", stack.Peek());
+
+            while(stack.Count > 0)
+            {
+                Console.WriteLine("Popped item: {0}", stack.Pop());
+            }
+
+            //reversing an int array
+            Stack<int> myStack = new Stack<int>();
+            int[] numbers=new int[] {8,2,3,4,7,6,2};
+
+            Console.WriteLine("The numbers in the array are: ");
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+                myStack.Push(number);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Reversed numbers: ");
+            while (myStack.Count > 0)
+            {
+                Console.Write("{0} ", myStack.Pop());
+            }
+
         }
     }
 }
