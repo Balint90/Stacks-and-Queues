83eb779e0c334881e31940377472bec6972ec092
balint.csertan <balint.csertan@pazirik.hu>
2021-12-04 14:43:21 +0100

    Add project files.

---
+++ Stacks and Queues/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stacks_and_Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            Console.WriteLine("Top value in the stack is: {0}", stack.Peek());
+            stack.Push(2);
+            Console.WriteLine("Top value in the stack is: {0}", stack.Peek());
+            stack.Push(3);
+            Console.WriteLine("Top value in the stack is: {0}", stack.Peek());
+        }
+    }
+}
